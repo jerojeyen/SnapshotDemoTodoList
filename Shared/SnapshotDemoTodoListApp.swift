fa9fceab06ebdb09d5397a23c5c6a38f28a7a428
Jerozan Jeyendrarasa <me@jerojeyen.com>
2021-02-25 16:20:34 +0100

    Initial Commit

---
+++ Shared/SnapshotDemoTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotDemoTodoListApp.swift
+//  Shared
+//
+//  Created by Jerozan Jeyendrarasa on 25/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotDemoTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
